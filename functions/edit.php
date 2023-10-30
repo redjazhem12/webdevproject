995f55a24cb2cdb69dbfa196110c300eda52f8eb
Jeika Lago <jeikabryll2020@gmail.com>
2023-10-30 14:38:50 +0800

    edit functions

---
+++ functions/edit.php
@@ -0,0 +1,25 @@
+<?php
+    
+    // including the database connection file
+    include_once("../dbConnection/mysqlconfig_connection.php"); 
+    if(isset($_POST['update'])) {
+        $id = $_POST['id']; 
+        $code=$_POST['code']; 
+        $name=$_POST['name'];
+        // checking empty fields
+        if (empty($code) || empty($name)) {
+            if (empty($code)) {
+                echo "<font color='red'>Subject Code field is empty.</font><br/>";
+            }
+            
+            if (empty($name)) {
+                echo "<font color='red'>Subject Name field is empty.</font><br/>";
+            }
+        }
+        else {
+            //updating the table
+            mysqli_query($dbc, "UPDATE tblsubject SET subject_code='$code', subject_name='$name' WHERE subject_id='$id");
+            header("Location: ../index.php");
+        }
+    }
+?>
